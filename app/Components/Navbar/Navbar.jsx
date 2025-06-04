330f80d8dc5cc4231efca0444b9d8ddcb5d466f9
Chripssan <arunchripssan@gmail.com>
2025-06-04 17:47:51 +0530

    fixes  - Navbar active page indication  - Logo home page redirection

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
@@ -11,15 +12,16 @@ import "./Navbar.scss";
 const Navbar = () => {
     const [aboutAnchorEl, setAboutAnchorEl] = useState(null);
     const [projectsAnchorEl, setProjectsAnchorEl] = useState(null);
+    const pathname = usePathname();
 
     const handleAboutClick = (event) => {
         setAboutAnchorEl(event.currentTarget);
-        setProjectsAnchorEl(null); 
+        setProjectsAnchorEl(null);
     };
 
     const handleProjectsClick = (event) => {
         setProjectsAnchorEl(event.currentTarget);
-        setAboutAnchorEl(null); 
+        setAboutAnchorEl(null);
     };
 
     const handleClose = () => {
@@ -32,45 +34,100 @@ const Navbar = () => {
             <nav className="nav">
                 <div className="wrapper">
                     <div className="container">
-                        <Image src="/assets/logo.png" alt="" width={180} height={50} className="logo" />
+                        <a href="/">
+                            <Image
+                                href="/"
+                                src="/assets/logo.png"
+                                alt=""
+                                width={180}
+                                height={50}
+                                className="logo"
+                            />
+                        </a>
                         <ul className="nav-list">
-                            <li className="nav-item">
-                                <Link className="nav-link" href="/">HOME</Link>
+                            <li
+                                className={`nav-item ${
+                                    pathname === "/" ? "active" : ""
+                                }`}
+                            >
+                                <Link className="nav-link" href="/">
+                                    HOME
+                                </Link>
                             </li>
 
                             <li
-                                className="nav-item dropDownItem"
+                                className={`nav-item  ${
+                                    pathname.startsWith("/about") ||
+                                    pathname.startsWith("/awards")
+                                        ? "active"
+                                        : ""
+                                }`}
                                 onClick={handleAboutClick}
                             >
-                                ABOUT US
-                                <Image src="/assets/icons/down-arrow.png" alt="" width={16} height={16} className="downArrow" />
+                                <div className="dropDownItem">
+                                    ABOUT US
+                                    <Image
+                                        src="/assets/icons/down-arrow.png"
+                                        alt=""
+                                        width={16}
+                                        height={16}
+                                        className="downArrow"
+                                    />
+                                </div>
                             </li>
                             <Menu
                                 anchorEl={aboutAnchorEl}
                                 open={Boolean(aboutAnchorEl)}
                                 onClose={handleClose}
-                                 sx={{
+                                sx={{
                                     "& .MuiPaper-root": {
                                         backgroundColor: "#33b9ed",
                                         color: "white",
-                                        fontFamily: "RacingSansOne"
+                                        fontFamily: "RacingSansOne",
                                     },
                                 }}
                             >
-                                <MenuItem onClick={handleClose}><Link className="nav-link" href="/about">WHO WE ARE</Link></MenuItem>
-                                <MenuItem onClick={handleClose}><Link className="nav-link" href="/awards">AWARDS & CERTIFICATION</Link></MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    <Link className="nav-link" href="/about">
+                                        WHO WE ARE
+                                    </Link>
+                                </MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    <Link className="nav-link" href="/awards">
+                                        AWARDS & CERTIFICATION
+                                    </Link>
+                                </MenuItem>
                             </Menu>
 
-                            <li className="nav-item">
-                                <Link className="nav-link" href="/services">SERVICES</Link>
+                            <li
+                                className={`nav-item ${
+                                    pathname === "/services" ? "active" : ""
+                                }`}
+                            >
+                                <Link className="nav-link" href="/services">
+                                    SERVICES
+                                </Link>
                             </li>
 
                             <li
-                                className="nav-item dropDownItem"
+                                className={`nav-item ${
+                                    pathname.startsWith("/completedProjects") ||
+                                    pathname.startsWith("/onGoingProjects")
+                                        ? "active"
+                                        : ""
+                                }`}
                                 onClick={handleProjectsClick}
                             >
-                                PROJECTS
-                                <Image src="/assets/icons/down-arrow.png" alt="" width={16} height={16} className="downArrow" />
+                                <div className="dropDownItem">
+                                    PROJECTS
+                                    <Image
+                                        src="/assets/icons/down-arrow.png"
+                                        alt=""
+                                        width={16}
+                                        height={16}
+                                        className="downArrow"
+                                    />
+                                </div>
                             </li>
                             <Menu
                                 anchorEl={projectsAnchorEl}
@@ -80,17 +137,51 @@ const Navbar = () => {
                                     "& .MuiPaper-root": {
                                         backgroundColor: "#33b9ed",
                                         color: "white",
-                                        fontFamily: "RacingSansOne"
+                                        fontFamily: "RacingSansOne",
                                     },
                                 }}
                             >
-                                <MenuItem onClick={handleClose}><Link className="nav-link" href="/completedProjects">COMPLETED PROJECTS</Link></MenuItem>
-                                <MenuItem onClick={handleClose}><Link className="nav-link" href="/onGoingProjects">ONGOING PROJECTS</Link></MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    <Link
+                                        className="nav-link"
+                                        href="/completedProjects"
+                                    >
+                                        COMPLETED PROJECTS
+                                    </Link>
+                                </MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    <Link
+                                        className="nav-link"
+                                        href="/onGoingProjects"
+                                    >
+                                        ONGOING PROJECTS
+                                    </Link>
+                                </MenuItem>
                             </Menu>
 
-                            <li className="nav-item"><Link className="nav-link" href="/ourClients">OUR CLIENTS</Link></li>
-                            <li className="nav-item"><Link className="nav-link" href="/careers">CAREERS</Link></li>
-                            <li className="nav-item"><Link className="nav-link" href="">PROFILE</Link></li>
+                            <li
+                                className={`nav-item ${
+                                    pathname === "/ourClients" ? "active" : ""
+                                }`}
+                            >
+                                <Link className="nav-link" href="/ourClients">
+                                    OUR CLIENTS
+                                </Link>
+                            </li>
+                            <li
+                                className={`nav-item ${
+                                    pathname === "/careers" ? "active" : ""
+                                }`}
+                            >
+                                <Link className="nav-link" href="/careers">
+                                    CAREERS
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="">
+                                    PROFILE
+                                </Link>
+                            </li>
                         </ul>
                         <Link href="/contact">
                             <button className="contact">Contact Us</button>
